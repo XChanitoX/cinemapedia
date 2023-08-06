c1744c2f475ad3de1ba10d35a946a5030fefddda
XChanitoX <c.salazar@aprendercreando.com.pe>
2023-08-05 23:40:17 -0500

    configurando las peticiones URL

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,6 +1,6 @@
 // Definimos cómo lucen los origenes de datos que pueden traer películas
 import 'package:cinemapedia/domain/entities/movie.dart';
 
-abstract class MovieDatasource {
+abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
