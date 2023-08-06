e10b2d49bf9baef631d83dbe9f27c343b4899fe4
XChanitoX <c.salazar@aprendercreando.com.pe>
2023-08-06 18:52:53 -0500

    evitar peticiones simultaneas

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -16,15 +16,23 @@ typedef MovieCallback = Future<List<Movie>> Function({int page});
 // El StateNotifier es una clase que sirve para manejar este estado
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isLoading =
+      false; // Bandera para avisar si ya se hizo una petición de otra página de películas
   MovieCallback fetchMoreMovies;
 
   MoviesNotifier({required this.fetchMoreMovies}) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+
     currentPage++;
 
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
 
     state = [...state, ...movies];
+
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
   }
 }
