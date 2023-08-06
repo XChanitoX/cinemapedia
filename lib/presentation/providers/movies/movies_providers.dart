e9eeb809bd96b9055b6fb1dbcd317f11c9cf6538
XChanitoX <c.salazar@aprendercreando.com.pe>
2023-08-06 01:15:45 -0500

    Now Playing Provider y Notifier

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,30 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+// El StateNotifierProvider es un proveedor de un etsaod que notifica su cambio
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+// Esto es para definir el caso de uso de obtener las películas
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+// El StateNotifier es una clase que sirve para manejar este estado
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({required this.fetchMoreMovies}) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+
+    state = [...state, ...movies];
+  }
+}
