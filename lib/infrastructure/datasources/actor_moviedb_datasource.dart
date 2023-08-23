3d5785eb28e2f32176bfd84ffb46860149f9510c
XChanitoX <c.salazar@aprendercreando.com.pe>
2023-08-23 14:42:15 -0500

    mappers e implementaciones de actores

---
+++ lib/infrastructure/datasources/actor_moviedb_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:cinemapedia/infrastructure/mappers/actor_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+import 'package:dio/dio.dart';
+
+import 'package:cinemapedia/config/constants/environment.dart';
+
+import 'package:cinemapedia/domain/datasources/actors_datasource.dart';
+import 'package:cinemapedia/domain/entities/actor.dart';
+
+class ActorMovieDbDatasource extends ActorsDatasource {
+  // Se configura el URL al que se realizan las peticiones
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Actor>> getActorsByMovie(String movieId) async {
+    final response = await dio.get('/movie/$movieId/credits');
+
+    final castResponse = CreditsResponse.fromJson(response.data);
+
+    List<Actor> actors = castResponse.cast
+        .map((cast) => ActorMapper.castToEntity(cast))
+        .toList();
+
+    return actors;
+  }
+}
---
+++ lib/infrastructure/mappers/actor_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+
+class ActorMapper {
+  static Actor castToEntity(Cast cast) => Actor(
+      id: cast.id,
+      name: cast.name,
+      profilePath: cast.profilePath != null
+          ? 'https://image.tmdb.org/t/p/w500${cast.profilePath}'
+          : 'https://blogtimenow.com/wp-content/uploads/2014/06/hide-facebook-profile-picture-notification.jpg',
+      character: cast.character);
+}
